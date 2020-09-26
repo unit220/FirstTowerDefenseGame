430466a5024e01856eff3b414b49201f194e5635
BennettSMarra <bmarra@email.sc.edu>
2020-09-26 14:10:50 -0400

    Add bullet and bullet impact effect
    Adds a bullet that is fired out of the tower and targets an enemy. Once the bullet reaches the enemy it is destroyed in a small partilcle effect.

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
+
+    [Header("Attributes")]
+
     public float range = 15f;
+    public float firerate = 1f;
+    private float fireCountdown = 0f;
+
+    [Header("Unity Setup Fields")]
 
     public string enemyTag = "Enemy";
 
     public Transform partToRotate;
     public float turnSpeed = 10f;
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     // Start is called before the first frame update
     void Start() {
@@ -52,6 +62,23 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(dir);
         Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime*turnSpeed).eulerAngles;
         partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f); ;
+
+        if (fireCountdown <= 0f) {
+            Shoot();
+            fireCountdown = 1f / firerate;
+        }
+
+        fireCountdown -= Time.deltaTime;
+
+    }
+
+    void Shoot() {
+        GameObject bulletGO = (GameObject) Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Bullet bullet = bulletGO.GetComponent<Bullet>();
+
+        if (bullet != null) {
+            bullet.Seek(target);
+        }
     }
 
     private void OnDrawGizmosSelected() {
