cd460ee04f73d24901b4e44d42a5e9bb94a23427
BennettSMarra <bmarra@email.sc.edu>
2020-09-26 15:39:39 -0400

    Add node highlight/building and Build Manager
    Adds a MyNode script that causes nodes to light up when moused over and allows the buildilng of turrets on any node. Also adds a Build Manager script that controls where you can build towers and blocks you from overlapping them.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake() {
+        if (instance != null) {
+            Debug.Log("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+
+    public GameObject standardTurretPrefab;
+
+    void Start() {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild() {
+        return turretToBuild;
+    }
+}
---
+++ Assets/Scripts/MyNode.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class MyNode : MonoBehaviour
+{
+
+    public Color hoverColor;
+    public Vector3 positionOffset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start() {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown() {
+        if (turret != null) {
+            Debug.Log("Can't build there! - TODO: Dispaly on screen.");
+            return;
+        }
+
+        // Build a turret
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position + positionOffset, transform.rotation);
+    }
+
+    private void OnMouseEnter() {
+        rend.material.color = hoverColor;
+    }
+
+    private void OnMouseExit() {
+        rend.material.color = startColor;
+    }
+}
