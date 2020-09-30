64bee281d3476e39d5b1e23f87ffe0f0cbdeb297
BennettSMarra <bmarra@email.sc.edu>
2020-09-30 00:57:52 -0400

    Add particles to building
    Adds a gold particle effect when building a tower.

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -16,6 +16,8 @@ public class BuildManager : MonoBehaviour
     public GameObject standardTurretPrefab;
     public GameObject anotherTurretPrefab;
 
+    public GameObject buildEffect;
+
     private TurretBlueprint turretToBuild;
 
     public bool CanBuild { get { return turretToBuild != null; } }
@@ -34,6 +36,9 @@ public class BuildManager : MonoBehaviour
         //GameObject turretToBuild = buildManager.GetTurretToBuild();
         GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.turret = turret;
+
+        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
 
         Debug.Log("Turret build! Money left: " + PlayerStats.Money);
     }
