99860d86cd2fc873a9c6a254ea6e9863cd5c008c
BennettSMarra <bmarra@email.sc.edu>
2020-09-30 18:40:12 -0400

    Add enemy HP and money on kill

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,32 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 10f;
 
+    public int health = 100;
+
+    public int value = 50;
+
+    public GameObject deathEffect;
+
     private Transform target;
     private int wavepointIndex = 0;
 
     private void Start()
     {
         target = Waypoints.points[0];
+    }
+
+    public void TakeDamage (int amount) {
+        health -= amount;
+        if (health <= 0) {
+            Die();
+        }
+    }
+
+    void Die () {
+        PlayerStats.Money += value;
+        GameObject effect = (GameObject)Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 5f);
+        Destroy(gameObject);
     }
 
     private void Update()
@@ -30,12 +50,17 @@ public class Enemy : MonoBehaviour
         // Enemy has reached the end
         if (wavepointIndex >= Waypoints.points.Length - 1)
         {
-            Destroy(gameObject);
+            EndPath();
             return; // makes sure the code doesn't skip into next node segment (yes this happens)
         }
 
         // Not at the end, find next waypoint
         wavepointIndex++;
         target = Waypoints.points[wavepointIndex];
+    }
+
+    void EndPath () {
+        PlayerStats.Lives--;
+        Destroy(gameObject);
     }
 }
